e3ab020d199e98bd1377bad5ab77e770ef3265f1
DanielTelesdeOliveira <daniel.wteles@gmail.com>
2026-09-13 17:26:42 -0300

    GREEN: implementa o bloqueio de reservas sem hóspedes

--- hotel/modelo/Apartamento.java
+++ hotel/modelo/Apartamento.java
@@ -22,8 +22,12 @@ public class Apartamento {
      * @post Se bem-sucedido, o apartamento terá o status RESERVADO e o hospede será armazenado
      */
     public void reservar(Hospede h) {
-        this.status = Status.RESERVADO;
-        this.hospede = h;
+        if(h != null){
+            this.status = Status.RESERVADO;
+            this.hospede = h;
+        } else {
+            throw new IllegalArgumentException("É necessário haver um hóspede para realizar a reserva.");
+        }
     }
 
     public void checkin(Hospede h) {
