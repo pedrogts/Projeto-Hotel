6a24759f8830a623dfdd3521f8ba3fda03695783
Rubens <rubenssexto@hotmail.com>
2026-09-13 14:43:43 -0300

    GREEN: implementa o minimo para testarFalhaCheckinEmAptoOcupado() funcionar

--- hotel/modelo/Apartamento.java
+++ hotel/modelo/Apartamento.java
@@ -27,6 +27,9 @@ public class Apartamento {
     }
 
     public void checkin(Hospede h) {
+        if (status == Status.OCUPADO){
+            throw new IllegalStateException("Nao e possivel fazer checkin em apartamento ocupado");
+        }
         this.hospede = h;
         this.status = Status.OCUPADO;
         //throw new UnsupportedOperationException("Implementar: LIVRE/RESERVADO -> OCUPADO");
