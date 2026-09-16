695c0f88e0d4cd5c304a41a3a5284656758d62e7
Rubens <rubenssexto@hotmail.com>
2026-09-16 18:22:38 -0300

    REFACTOR: correcao em Hospede

--- hotel/modelo/Hospede.java
+++ hotel/modelo/Hospede.java
@@ -11,7 +11,6 @@ import java.io.Serializable;
  */
 public class Hospede implements Serializable {
 
-    private static final long serialVersionUID = 1L;
 
     private final String cpf;
     private final String nome;
